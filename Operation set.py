90ecb71a907dcfe3c9b028193c93e3a7663d86e2
Meghana931 <86868270+Meghana931@users.noreply.github.com>
2022-10-15 20:22:54 +0530

    Add files via upload

---
+++ Operation set.py
@@ -0,0 +1,14 @@
+#define three sets
+M = {1, 2, 3, 4, 5};
+U = {2, 4, 6, 8, 10};
+#set union 
+print("Union of M and U is",M | U)
+
+#set intersecton
+print("intersection of M nd U is",M & U)
+
+#set difference
+print("Difference of M and U is",M - U)
+
+#set symmetric difference
+print("Symmetric difference of M and U is",M ^ U)

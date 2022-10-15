4a5604928f61d3368850e7d2760688eed210dfd1
Meghana931 <86868270+Meghana931@users.noreply.github.com>
2022-10-15 19:57:34 +0530

    Add files via upload

---
+++ fib sequence.py
@@ -0,0 +1,20 @@
+# phyton program for a fibnocci squence
+n = int(input("enter the length "))
+def fib(n):
+
+  #first two terms
+    a,b = 0,1
+     
+    if n == 1:
+        print(a)
+        
+    else:
+            print(a)
+            print(b)
+            
+            for i in range(2,n):
+                c = a+b
+                a = b
+                b = c
+                print(c)
+fib(n)
